35be93788fa65ec1264a6178c84d7f62549404b9
Placidia <placidia.ong@me.com>
2025-07-23 13:10:39 -0700

    Added my picture, my name, and a yellow box for description

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -14,7 +14,23 @@ struct ContentView: View {
                 .resizable()
                 .frame (width: 400, height: 1500)
             VStack {
-                
+                Image("me")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width:300)
+                    .offset()
+                Text("Placidia Ong")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                ZStack {
+                    Rectangle()
+                        .padding(.bottom, 2.0)
+                        .frame(width:360, height: 200)
+                        .offset(y:50)
+                        .foregroundColor(Color(red: 1.0, green: 1.0, blue: 0.5))
+                    
+                }
             }
         }
         .padding()
